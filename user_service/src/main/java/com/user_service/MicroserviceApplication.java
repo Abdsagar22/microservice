5e2f18c27d2c5ae8d4c277907b70b1b4dff7a34a
sagar <sagar.a.ingale03@gmail.com>
2024-03-09 16:43:56 +0530

    logger is added

--- user_service/src/main/java/com/user_service/MicroserviceApplication.java
+++ user_service/src/main/java/com/user_service/MicroserviceApplication.java
@@ -3,9 +3,11 @@ package com.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
--- user_service/src/main/java/com/user_service/entity/Rating.java
+++ user_service/src/main/java/com/user_service/entity/Rating.java
@@ -6,6 +6,32 @@ public class Rating {
 	private String userId;
 	private String hotelId;
 	private String rating;
+	public String getRatingId() {
+		return ratingId;
+	}
+	public void setRatingId(String ratingId) {
+		this.ratingId = ratingId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getHotelId() {
+		return hotelId;
+	}
+	public void setHotelId(String hotelId) {
+		this.hotelId = hotelId;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	
+	
 
 
 }
